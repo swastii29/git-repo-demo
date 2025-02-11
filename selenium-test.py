e69ba2943dbf1de45f421f5cafd1159df7a47a8b
Neha Mahato <mahatoneha837@gmail.com>
2025-02-11 14:38:23 +0545

    initial commit

--- selenium-test.py
+++ selenium-test.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Edge()
-driver.get('https://bing.com')
+browser = webdriver.Edge()
+browser.get('https://bing.com')
+
+time.sleep(2)
+
+SearchBar = browser.find_element(By.ID, 'sb_form_q')
+SearchBar.send_keys('Mahendra Morang Campus Biratnagar')
+SearchBar.submit()
 
 time.sleep(5)
-
-element = driver.find_element(By.ID, 'sb_form_q')
-element.send_keys('Quantum Infosys Pvt. Ltd Biratnagar')
-element.submit()
-
-time.sleep(5)
-driver.quit()
+browser.quit()
