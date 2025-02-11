140b3c885035c57a68e4643dee34ba95bad03527
Parbat Bhattarai <parbat.1993@gmail.com>
2025-02-11 14:36:53 +0545

    print listed jobs during loop

--- job-search.py
+++ job-search.py
@@ -4,14 +4,23 @@ import time
 
 browser = webdriver.Edge()
 
-jobList = ["QA", "PHP", "Java"]
+jobList = ["QA", "PHP", "Java", "Python"]
 
 for job in jobList:
+    print(job)
     browser.get('https://merojob.com')
     searchBar = browser.find_element(By.ID, 'job_search')
     searchBar.send_keys(job)
     searchBar.submit()
-    time.sleep(5)
+
+    # Find all <a> elements with href attributes
+    links = browser.find_elements(By.TAG_NAME, 'h1')
+
+    # Loop through each link and print the title attribute
+    for link in links:
+        print(link.text)
+
+    time.sleep(3)
 
 time.sleep(5)
 browser.quit()
