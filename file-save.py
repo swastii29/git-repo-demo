2775c2cedcd89e251d8c43ef79b551d2b1f3df05
Neha Mahato <mahatoneha837@gmail.com>
2025-02-12 13:30:32 +0545

    initial commit

---
+++ file-save.py
@@ -0,0 +1,7 @@
+x = 4
+x = "Neha"
+print(x)
+file = open('test.txt', 'w') 
+file.write(x) 
+file.close() 
+
--- job-search.py
+++ job-search.py
@@ -2,9 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-browser = webdriver.Edge()
+from selenium.webdriver.edge.options import Options
+
+options = Options()
+options.add_argument("--headless=new")
+
+browser = webdriver.Edge(options = options)
+
+
+
 
 jobList = ["QA", "PHP", "Java", "Python"]
+my_string = ""
 
 for job in jobList:
     print(job)
@@ -16,11 +25,20 @@ for job in jobList:
     # Find all <a> elements with href attributes
     links = browser.find_elements(By.TAG_NAME, 'h1')
 
+    my_string +=job+"\n"
+
+
     # Loop through each link and print the title attribute
     for link in links:
         print(link.text)
-
-    time.sleep(3)
+        my_string +=link.text
+        my_string +="\n"
+    my_string +="\n"      
+time.sleep(3)
+print(my_string)
+file = open('test.txt', 'w') 
+file.write(my_string) 
+file.close() 
 
 time.sleep(5)
 browser.quit()
